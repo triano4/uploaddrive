3221c2e2f7f58adae58746bd38c214613a17841f
syahjamal <sulaimansyahjamal@gmail.com>
2020-11-10 16:22:15 +0700

    http handler

--- go.mod
+++ go.mod
@@ -5,6 +5,7 @@ go 1.15
 require (
 	github.com/emersion/go-imap v1.0.6
 	github.com/emersion/go-message v0.13.0
+	github.com/gorilla/mux v1.8.0
 	golang.org/x/oauth2 v0.0.0-20201109201403-9fd604954f58
 	golang.org/x/text v0.3.3
 	google.golang.org/api v0.35.0
--- main.go
+++ main.go
@@ -1,11 +1,20 @@
 package main
 
 import (
+	"fmt"
+	"net/http"
+
+	"github.com/gorilla/mux"
 	"github.com/triano4/uploaddrive/code"
 )
 
 func main() {
+	r := mux.NewRouter()
 
-	code.Client()
+	http.Handle("/", r)
+	r.HandleFunc("/file", code.Client)
+	// code.Client()
 
+	fmt.Println("Serve localhost:9000")
+	http.ListenAndServe(":9000", r)
 }
