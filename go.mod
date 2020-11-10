2132c0809f4c4dc3e889e3a3c8fadbbf1872a776
trianom4 <trianom4@gmail.com>
2020-11-10 11:31:05 +0700

    first commit

---
+++ go.mod
@@ -0,0 +1,17 @@
+module uploaddrive
+
+go 1.15
+
+require (
+	cloud.google.com/go v0.69.1 // indirect
+	github.com/emersion/go-imap v1.0.6
+	github.com/emersion/go-message v0.13.0
+	github.com/golang/protobuf v1.4.3 // indirect
+	golang.org/x/net v0.0.0-20201016165138-7b1cca2348c0
+	golang.org/x/oauth2 v0.0.0-20200902213428-5d25da1a8d43
+	golang.org/x/sys v0.0.0-20201018230417-eeed37f84f13 // indirect
+	google.golang.org/api v0.33.0
+	google.golang.org/appengine v1.6.7 // indirect
+	google.golang.org/genproto v0.0.0-20201015140912-32ed001d685c // indirect
+	google.golang.org/grpc v1.33.0 // indirect
+)
---
+++ main.go
@@ -0,0 +1,272 @@
+package main
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"golang.org/x/oauth2"
+	"golang.org/x/oauth2/google"
+	"google.golang.org/api/drive/v3"
+	"io"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/emersion/go-imap"
+	"github.com/emersion/go-imap/client"
+	"github.com/emersion/go-message"
+)
+
+func main() {
+
+	Client()
+	
+
+}
+
+
+ // ------------------------------------------------------------------------------------------------------
+ // Retrieve a token, saves the token, then returns the generated client.
+ func getClient(config *oauth2.Config) *http.Client {
+	 // The file token.json stores the user's access and refresh tokens, and is
+	 // created automatically when the authorization flow completes for the first
+	 // time.
+	 tokFile := "config/token.json"
+	 tok, err := tokenFromFile(tokFile)
+	 if err != nil {
+		tok = getTokenFromWeb(config)
+		saveToken(tokFile, tok)
+	 }
+	 return config.Client(context.Background(), tok)
+  }
+  
+  // Request a token from the web, then returns the retrieved token.
+  func getTokenFromWeb(config *oauth2.Config) *oauth2.Token {
+	 authURL := config.AuthCodeURL("state-token", oauth2.AccessTypeOffline)
+	 fmt.Printf("Go to the following link in your browser then type the "+
+		"authorization code: \n%v\n", authURL)
+  
+	 var authCode string
+	 if _, err := fmt.Scan(&authCode); err != nil {
+		log.Fatalf("Unable to read authorization code %v", err)
+	 }
+  
+	 tok, err := config.Exchange(context.TODO(), authCode)
+	 if err != nil {
+		log.Fatalf("Unable to retrieve token from web %v", err)
+	 }
+	 return tok
+  }
+  
+  // Retrieves a token from a local file.
+  func tokenFromFile(file string) (*oauth2.Token, error) {
+	 f, err := os.Open(file)
+	 if err != nil {
+		return nil, err
+	 }
+	 defer f.Close()
+	 tok := &oauth2.Token{}
+	 err = json.NewDecoder(f).Decode(tok)
+	 return tok, err
+  }
+  
+  //-----------------------------------------------------------------------------------------------------
+  // Saves a token to a file path.
+  func saveToken(path string, token *oauth2.Token) {
+	 fmt.Printf("Saving credential file to: %s\n", path)
+	 f, err := os.OpenFile(path, os.O_RDWR|os.O_CREATE|os.O_TRUNC, 0600)
+	 if err != nil {
+		log.Fatalf("Unable to cache oauth token: %v", err)
+	 }
+	 defer f.Close()
+	 json.NewEncoder(f).Encode(token)
+  }
+ 
+  func getService() (*drive.Service, error) {
+	 b, err := ioutil.ReadFile("config/credentials.json")
+	 if err != nil {
+		fmt.Printf("Unable to read credentials.json file. Err: %v\n", err)
+		return nil, err
+	 }
+  
+	 // If modifying these scopes, delete your previously saved token.json.
+	 config, err := google.ConfigFromJSON(b, drive.DriveFileScope)
+  
+	 if err != nil {
+		return nil, err
+	 }
+  
+	 client := getClient(config)
+  
+	 service, err := drive.New(client)
+  
+	 if err != nil {
+		fmt.Printf("Cannot create the Google Drive service: %v\n", err)
+		return nil, err
+	 }
+  
+	 return service, err
+  }
+ 
+ //-----------------------------------------------------------------------------------------------------
+ //  create directory  in drive
+ func createDir(service *drive.Service, name string, parentId string) (*drive.File, error) {
+	 d := &drive.File{
+		Name:     name,
+		MimeType: "application/vnd.google-apps.folder",
+		Parents:  []string{parentId},
+	 }
+  
+	 file, err := service.Files.Create(d).Do()
+  
+	 if err != nil {
+		log.Println("Could not create dir: " + err.Error())
+		return nil, err
+	 }
+  
+	 return file, nil
+  }
+ 
+ //------------------------------------------------------------------------------------------------------
+ //  create file to upload to drive
+ func createFile(service *drive.Service, name string, mimeType string, content io.Reader, parentId string) (*drive.File, error) {
+	f := &drive.File{
+	 MimeType: mimeType,
+	 Name:     name,
+	 Parents:  []string{parentId},
+  }
+  file, err := service.Files.Create(f).Media(content).Do()
+ 
+  if err != nil {
+	 log.Println("Could not create file: " + err.Error())
+	 return nil, err
+  }
+ 
+  return file, nil
+ }
+ // -------------------------------------------------------------------------------------------------------
+ 
+ func uploadFile(folderName string, fileName string){
+	 
+	 // Step 1. Open the file
+	 f, err := os.Open(fileName)
+  
+	 if err != nil {
+		panic(fmt.Sprintf("cannot open file: %v", err))
+	 }
+  
+	 defer f.Close()
+  
+	 // Step 2. Get the Google Drive service
+	 service, err := getService()
+  
+	 // Step 3. Create the directory
+	 dir, err := createDir(service, folderName, "root")
+  
+	 if err != nil {
+		panic(fmt.Sprintf("Could not create dir: %v\n", err))
+	 }
+ 
+	//  contentType, err := GetFileContentType(f)
+	//  if err != nil {
+	// 	 panic(err)
+	//  }
+  
+	 // Step 4. Create the file and upload its content
+	 file, err := createFile(service, fileName, "application/pdf", f, dir.Id)
+  
+	 if err != nil {
+		panic(fmt.Sprintf("Could not create file: %v\n", err))
+	 }
+  
+	 fmt.Printf("File '%s' successfully uploaded in '%s' directory", file.Name,  dir.Name)
+ }
+
+//Client Function
+func Client() {
+	log.Println("Connecting to server...")
+
+	c, err := client.DialTLS("imap.gmail.com:993", nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	log.Println("Connected")
+
+	defer c.Logout()
+
+	if err := c.Login("trianom4@gmail.com", "akuberuntung"); err != nil {
+		log.Fatal(err)
+	}
+	log.Println("Logged in")
+
+	mbox, err := c.Select("INBOX", false)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	log.Println(mbox.Messages)
+
+	criteria := imap.NewSearchCriteria()
+	criteria.WithoutFlags = []string{"\\Seen"}
+
+	uids, err := c.Search(criteria)
+	if err != nil {
+		log.Println(err)
+	}
+
+	seqset := new(imap.SeqSet)
+	seqset.AddNum(uids...)
+
+	messages := make(chan *imap.Message, 10)
+	done := make(chan error, 1)
+	go func() {
+		done <- c.Fetch(seqset, []imap.FetchItem{imap.FetchRFC822, imap.FetchEnvelope, imap.FetchFlags, imap.FetchInternalDate}, messages)
+	}()
+
+	for msg := range messages {
+		for _, r := range msg.Body {
+			entity, err := message.Read(r)
+			if err != nil {
+				log.Fatal(err)
+			}
+
+			multiPartReader := entity.MultipartReader()
+
+			for e, err := multiPartReader.NextPart(); err != io.EOF; e, err = multiPartReader.NextPart() {
+				kind, params, cErr := e.Header.ContentType()
+				if cErr != nil {
+					log.Fatal(cErr)
+				}
+
+				if kind != "application/pdf" {
+					continue
+				}
+
+				c, rErr := ioutil.ReadAll(e.Body)
+				if rErr != nil {
+					log.Fatal(rErr)
+				}
+
+				log.Printf("Dump file %s", params["name"])
+				
+				if fErr := ioutil.WriteFile(params["name"], c, 0777); fErr != nil {
+					log.Fatal(fErr)
+				}
+				var filenams = params["name"]
+				
+				uploadFile("tesupload",filenams)
+				// log.Print(filenams)
+			}
+		}
+	}
+	
+	if err := <-done; err != nil {
+		log.Fatal(err)
+	}
+
+	log.Println("Done")
+
+
+}
